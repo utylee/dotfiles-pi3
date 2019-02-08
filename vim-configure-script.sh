1549c97d5a01ee87edec4585e50c3025b984b310
utylee <utylee@gmail.com>
2019-02-08 23:06:33 +0900

    vim-configure-script modi with-lua-prefix usr/local

--- vim-configure-script.sh
+++ vim-configure-script.sh
@@ -1,4 +1,4 @@
-./configure --with-features=huge --enable-multibyte --enable-rubyinterp=yes --enable-perlinterp=yes --enable-luainterp=yes --enable-pythoninterp=yes --with-python-config-dir=/usr/lib/python2.7/config-arm-linux-gnueabihf/ --enable-python3interp=yes --with-python3-config-dir=/home/pi/.pyenv/versions/3.6.5/lib/python3.6/config-3.6m-arm-linux-gnueabihf/ --enable-gui=gtk2 --enable-cscope
+./configure --with-features=huge --enable-multibyte --enable-rubyinterp=yes --enable-perlinterp=yes --enable-luainterp=yes --with-lua-prefix=/usr/local --enable-pythoninterp=yes --with-python-config-dir=/usr/lib/python2.7/config-arm-linux-gnueabihf/ --enable-python3interp=yes --with-python3-config-dir=/home/pi/.pyenv/versions/3.6.5/lib/python3.6/config-3.6m-arm-linux-gnueabihf/ --enable-gui=gtk2 --enable-cscope
 
 # ruby는 libruby-dev 형식이 아니고 ruby-dev 로 apt-get install 해야..
 # 나머지 include 파일은 sudo apt-get install libXXX-dev 형식이었다 대부분
