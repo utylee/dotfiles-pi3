0bc2016d8c12c0ff3a90640dd005b2d78aacc484
utylee <utylee@gmail.com>
2021-04-18 03:00:19 +0900

    vim-configure-script added for pi4 living room

--- vim-configure-script.sh
+++ vim-configure-script.sh
@@ -1,4 +1,6 @@
-./configure --with-features=huge --enable-multibyte --enable-rubyinterp=yes --enable-perlinterp=yes --enable-luainterp=yes --with-lua-prefix=/usr/local --enable-python3interp=yes --with-python3-config-dir=/home/pi/.pyenv/versions/3.7.2/lib/python3.7/config-3.7m-arm-linux-gnueabihf/ --enable-gui=gtk2 --enable-cscope
+./configure --with-features=huge --enable-multibyte --enable-rubyinterp=yes --enable-perlinterp=yes --enable-luainterp=yes --with-lua-prefix=/usr --enable-python3interp=yes --with-python3-config-dir=/home/pi/.pyenv/versions/3.8.9/lib/python3.8/config-3.8-arm-linux-gnueabihf/ --enable-gui=gtk2 --enable-cscope
+
+#./configure --with-features=huge --enable-multibyte --enable-rubyinterp=yes --enable-perlinterp=yes --enable-luainterp=yes --with-lua-prefix=/usr/local --enable-python3interp=yes --with-python3-config-dir=/home/pi/.pyenv/versions/3.7.2/lib/python3.7/config-3.7m-arm-linux-gnueabihf/ --enable-gui=gtk2 --enable-cscope
 
 # ruby는 libruby-dev 형식이 아니고 ruby-dev 로 apt-get install 해야..
 # 나머지 include 파일은 sudo apt-get install libXXX-dev 형식이었다 대부분
