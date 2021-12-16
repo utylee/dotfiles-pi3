fa63aadb16f0e2cc4fd8045566ddc5e186e96f74
utylee <utylee@gmail.com>
2021-12-17 03:34:05 +0900

    add: fish function smisync.fish srtsync.fish

---
+++ .config/fish/functions/smisync.fish
@@ -0,0 +1,5 @@
+function smisync
+    ~/.virtualenvs/misc/bin/python ~/.virtualenvs/misc/src/smi-sync.py $argv
+    #pysubs2 --to ssa *srt
+end
+
---
+++ .config/fish/functions/srtsync.fish
@@ -0,0 +1,5 @@
+function srtsync
+    ~/.virtualenvs/misc/bin/python ~/.virtualenvs/misc/src/srt-sync.py $argv
+    #pysubs2 --to ssa *srt
+end
+
