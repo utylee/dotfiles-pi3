d8661134af83d5397c5ca535665dfb5006957537
utylee <utylee@gmail.com>
2022-08-07 02:57:07 +0900

    added: fish functions srttosmi ssasync ssatosmi added

---
+++ .config/fish/functions/srttosmi.fish
@@ -0,0 +1,4 @@
+function srttosmi
+    #~/.virtualenvs/misc/bin/python ~/temp/smi2srt/smi2srt .
+    ~/.virtualenvs/misc/bin/python ~/.virtualenvs/misc/src/srt2smi.py .
+end
---
+++ .config/fish/functions/ssasync.fish
@@ -0,0 +1,5 @@
+function ssasync
+    ~/.virtualenvs/misc/bin/python ~/.virtualenvs/misc/src/ssa-sync.py $argv
+    #pysubs2 --to ssa *srt
+end
+
