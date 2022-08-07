ba46052cf721ed9c7e016bc98ecbcad6354f553d
utylee <utylee@gmail.com>
2022-08-08 02:18:17 +0900

    added: fish config function ssatosrt.fish added

---
+++ .config/fish/functions/ssatosrt.fish
@@ -0,0 +1,12 @@
+function ssatosrt
+    ~/.virtualenvs/misc/bin/python ~/.virtualenvs/misc/src/ssa2srt.py .
+end
+
+#smitosrt() {
+##~/.virtualenvs/misc/bin/python ~/temp/smi2srt/smi2srt .
+#~/.virtualenvs/misc/bin/python ~/.virtualenvs/misc/src/smi2srt.py .
+#}
+#srttosmi() {
+##~/.virtualenvs/misc/bin/python ~/temp/smi2srt/smi2srt .
+#~/.virtualenvs/misc/bin/python ~/.virtualenvs/misc/src/srt2smi.py .
+#}
